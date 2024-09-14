0644a7ac3af313ea29bbb730dfd609b3efb17255
Drawinakash0209 <drawinakash77@gmail.com>
2024-09-14 12:13:55 +0530

    sdg updated

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -5,8 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title', 'RAC')</title>
-    <meta name="description" content="@yield('meta-description', 'Default description')">
-    <meta name="keywords" content="@yield('meta-keywords', 'default, keywords')">
+    <meta name="description" content="Join the Rotaract Club of APIIT and be part of a dynamic community dedicated to making a difference. We focus on leadership, community service, and professional development, guided by Rotary International and the Rotary Club of Colombo East. Explore our projects, events, and how you can get involved.">
+<meta name="keywords" content="Rotaract Club, APIIT, community service, leadership, professional development, Rotary International, Rotary Club of Colombo East, youth empowerment, social impact, volunteering">
+
 
     @yield('meta')
     @yield('structured-data')
